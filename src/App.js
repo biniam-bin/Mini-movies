e631bcd421782e06976e22dcf2e711c3bdd6fbd7
biniam-bin <biniamdnl@gmail.com>
2021-07-13 09:21:46 -0700

    setup is ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { GlobalStyle } from "./GlobalStyle"
 
 function App() {
   return (
     <div className="App">
       Start here.
+      <GlobalStyle/>
     </div>
   );
 }
+
 
 export default App;
--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 // Read more about the API here: https://developers.themoviedb.org/
 
 const API_URL = 'https://api.themoviedb.org/3/';
-const API_KEY = 'bff86a8d55e82515f34df0a3ff667c46';
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const SEARCH_BASE_URL = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=`;
 const POPULAR_BASE_URL = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US`;
